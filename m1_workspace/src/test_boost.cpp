dd1bed0453ef456ca8b2357b9780af95358a5579
Rembrandt <nanachi1027@163.com>
2024-06-28 13:32:52 +0800

    Update test_boost.cpp

--- m1_workspace/src/test_boost.cpp
+++ m1_workspace/src/test_boost.cpp
@@ -2,7 +2,7 @@
 #include <boost/filesystem.hpp>
 
 int main() {
-    boost::filesystem::path path("./test_data.txt");
+    boost::filesystem::path path("/app/muduo");
     if (boost::filesystem::exists(path)) {
         std::cout << "Directory exists!" << std::endl;
     }
